bdc94bd97b895e9731d20ef1b0f036774cf5ec13
georgina6 <lunarecho530@gmail.com>
2023-10-25 17:26:20 +0800

    00014-easy-first

--- challenges/00014-easy-first /template.ts
+++ challenges/00014-easy-first /template.ts
@@ -1,12 +1,28 @@
-type First<T extends any[]> =T extends [] ? never : T[0]
+type First<T extends any[]> = T extends [infer First, ...infer Rest] ? First : never
 
-// 1.extends类型条件判断
-// 2.获取tuple的length属性
-// 3.extends union判断的规则
-// 4.inter的使用
+// 1.extends类型条件判断：T extends [] ? never : T[0]
+// 2.获取tuple的length属性：T['length'] extends 0 ? never : T[0]
+// 3.extends union判断的规则：T[0] extends T[number] ? T[0] : never
+// 4.infer(推断）：T extends [infer First, ...infer Rest] ? First : never
 
 // js
 // const first = (arr) => {
 //     // 空数组要返回never
+//     if (arr.length === 0) return 'never'
 //     return arr[0]
+// }
+
+// T[number]
+// type ages = [1,2,3]
+// // union
+// type t1 = ages[number]
+// // 看看某个值是不是在union里面
+// // 1 extends 1 / 1 extends 2 / 1 extends 3
+// type t2 = T[0] extends ages[number] ? 'true' : 'false'
+
+// 解构
+// const first = (arr) => {
+//     // 空数组要返回never
+//     const [first, ...rest] = arr
+//     return first ? first : 'never'
 // }
